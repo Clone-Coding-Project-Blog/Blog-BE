410e83aaa7bcf455db3e7a6d41d00d5438024467
MizzleNA <mizzleaaa@gmail.com>
2022-05-15 03:43:15 +0900

    :construction: mockmvc blogcontroller 진행중

--- blog-rest/src/test/java/com/mizzle/blogrest/controller/blog/BlogControllerTest.java
+++ blog-rest/src/test/java/com/mizzle/blogrest/controller/blog/BlogControllerTest.java
@@ -1,23 +1,30 @@
 package com.mizzle.blogrest.controller.blog;
 
+import com.mizzle.blogrest.domain.entity.blog.Purpose;
+import com.mizzle.blogrest.lib.JsonUtils;
+import com.mizzle.blogrest.payload.request.auth.SignInRequest;
+import com.mizzle.blogrest.payload.request.blog.CreateBoardRequest;
+import com.mizzle.blogrest.payload.request.blog.CreateReplyRequest;
 import com.mizzle.blogrest.service.blog.BoardService;
 
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import lombok.extern.slf4j.Slf4j;
 
-import static org.mockito.ArgumentMatchers.any; 
-import static org.mockito.BDDMockito.given; 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*; 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 @Slf4j
@@ -32,39 +39,233 @@ public class BlogControllerTest {
     @Autowired
     private BoardService boardService;
 
-    @Test
-    void testCreateBoard() {
+    private JSONObject signin(String email) throws Exception{
+        
+        SignInRequest signInRequest = new SignInRequest();
+        signInRequest.setEmail(email);
+        signInRequest.setPassword("string");
+
+        ResultActions actions = this.mockMvc.perform(
+                    post("/auth/signin")
+                    .content(
+                        JsonUtils.asJsonToString(signInRequest)
+                    )
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        JSONObject jsonObject = JsonUtils.asStringToJson(actions.andReturn().getResponse().getContentAsString());
+        return jsonObject;
+    }
+
+    private JSONObject createBoard() throws Exception {
+        //give
+        JSONObject token = signin("string@aa.bb");
+                
+        String accessToken = (String) token.get("accessToken");
+
+        List<String> tagNames = new ArrayList<>();
+        tagNames.add("tag1");
+        tagNames.add("tag2");
+
+        CreateBoardRequest createBoardRequest = CreateBoardRequest.builder()
+                                                                .title("sample")
+                                                                .subtitle("sample")
+                                                                .purpose(Purpose.finish)
+                                                                .markdown("markdown")
+                                                                .html("<p>markdown</p>")
+                                                                .tagNames(tagNames)
+                                                                .build();
+
+        //when
+        ResultActions actions = this.mockMvc.perform(
+                    post("/blog/board")
+                    .content(
+                        JsonUtils.asJsonToString(createBoardRequest)
+                    )
+                    .header("Authorization", String.format("Bearer %s", accessToken))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        //then
+        JSONObject jsonObject = JsonUtils.asStringToJson(actions.andReturn().getResponse().getContentAsString());
+        return (JSONObject) jsonObject.get("information");
         
     }
 
     @Test
-    void testCreateLike() {
+    void testCreateBoard() throws Exception {
+        //give
+        JSONObject token = signin("string@aa.bb");
+        
+        String accessToken = (String) token.get("accessToken");
+
+        List<String> tagNames = new ArrayList<>();
+        tagNames.add("tag1");
+        tagNames.add("tag2");
+
+        CreateBoardRequest createBoardRequest = CreateBoardRequest.builder()
+                                                                .title("sample")
+                                                                .subtitle("sample")
+                                                                .purpose(Purpose.finish)
+                                                                .markdown("markdown")
+                                                                .html("<p>markdown</p>")
+                                                                .tagNames(tagNames)
+                                                                .build();
+
+        //when
+        ResultActions actions = this.mockMvc.perform(
+                    post("/blog/board")
+                    .content(
+                        JsonUtils.asJsonToString(createBoardRequest)
+                    )
+                    .header("Authorization", String.format("Bearer %s", accessToken))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        //then
+        JSONObject jsonObject = JsonUtils.asStringToJson(actions.andReturn().getResponse().getContentAsString());
+        log.info("jsonObject={}",jsonObject);
 
     }
 
     @Test
-    void testCreateReply() {
+    void testCreateLike() throws Exception {
+        //give
+        JSONObject token = signin("string@aa.bb");
+        
+        String accessToken = (String) token.get("accessToken");
 
+        long boardId = 1;
+
+
+        //when
+        ResultActions actions = this.mockMvc.perform(
+                    post(String.format("/blog/like/%d", boardId))
+                    .header("Authorization", String.format("Bearer %s", accessToken))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        //then
+        JSONObject jsonObject = JsonUtils.asStringToJson(actions.andReturn().getResponse().getContentAsString());
+        log.info("jsonObject={}",jsonObject);
     }
 
     @Test
-    void testDeleteBoard() {
+    void testCreateReply() throws Exception{
+        //give
+        JSONObject token = signin("string@aa.bb");
+                
+        String accessToken = (String) token.get("accessToken");
 
+        long boardId = 1;
+
+        CreateReplyRequest createReplyRequest = new CreateReplyRequest();
+        createReplyRequest.setComment("testCreateLike");
+
+        //when
+        ResultActions actions = this.mockMvc.perform(
+                    post(String.format("/blog/reply/%d", boardId))
+                    .content(
+                        JsonUtils.asJsonToString(createReplyRequest)
+                    )
+                    .header("Authorization", String.format("Bearer %s", accessToken))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        //then
+        JSONObject jsonObject = JsonUtils.asStringToJson(actions.andReturn().getResponse().getContentAsString());
+        log.info("jsonObject={}",jsonObject);
     }
 
     @Test
-    void testDeleteLike() {
+    void testDeleteBoard() throws Exception {
+        JSONObject board = createBoard();
+        long boardId =  (long) board.get("id");
+        
+        JSONObject token = signin("string@aa.bb");
+        String accessToken = (String) token.get("accessToken");
 
+        //when
+        ResultActions actions = this.mockMvc.perform(
+                    delete(String.format("/blog/board/%d", boardId))
+                    .header("Authorization", String.format("Bearer %s", accessToken))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+        //then
+        JSONObject jsonObject = JsonUtils.asStringToJson(actions.andReturn().getResponse().getContentAsString());
+        log.info("jsonObject={}",jsonObject);
+    }
+
+    @Test
+    void testDeleteLike() throws Exception {
+        //give
+        JSONObject token = signin("string@aa.bb");
+        
+        String accessToken = (String) token.get("accessToken");
+
+        long boardId = 1;
+
+
+        //when
+        ResultActions actions = this.mockMvc.perform(
+                    delete(String.format("/blog/like/%d", boardId))
+                    .header("Authorization", String.format("Bearer %s", accessToken))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        //then
+        JSONObject jsonObject = JsonUtils.asStringToJson(actions.andReturn().getResponse().getContentAsString());
+        log.info("jsonObject={}",jsonObject);
     }
 
     @Test
     void testReadBoard() throws Exception {
-                    
+        //give
+        long boardId = 1;
+        //when
+        
+        ResultActions actions = this.mockMvc.perform(
+                        get(String.format("/blog/board/%d", boardId))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                    )
+                    .andExpect(status().isOk())
+                    .andDo(print());
+
+        //then
+        JSONObject jsonObject = JsonUtils.asStringToJson(actions.andReturn().getResponse().getContentAsString());
+        log.info("jsonObject={}",jsonObject);
+
     }
 
     @Test
     void testReadBoards() throws Exception {
-        this.mockMvc.perform(
+
+        //give
+        
+        //when
+        ResultActions actions = this.mockMvc.perform(
                         get("/blog/board")
                         .param("search", "")
                         .param("page", "0")
@@ -75,11 +276,29 @@ public class BlogControllerTest {
                     .andExpect(status().isOk())
                     .andDo(print());
 
+        //then
+        JSONObject jsonObject = JsonUtils.asStringToJson(actions.andReturn().getResponse().getContentAsString());
+        log.info("jsonObject={}",jsonObject);
+
     }
 
     @Test
-    void testReadReplys() {
+    void testReadReplys() throws Exception {
+        //give
+        long boardId = 1;
+        //when
+                
+        ResultActions actions = this.mockMvc.perform(
+                    get(String.format("/blog/board/%d", boardId))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
 
+        //then
+        JSONObject jsonObject = JsonUtils.asStringToJson(actions.andReturn().getResponse().getContentAsString());
+        log.info("jsonObject={}",jsonObject);
     }
 
     @Test
