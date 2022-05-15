03957d3e18c624803fd325dc29b04e481ae65430
MizzleNA <mizzleaaa@gmail.com>
2022-05-15 17:03:25 +0900

    :wrench: not null 추가

--- blog-rest/src/main/java/com/mizzle/blogrest/payload/request/auth/RefreshTokenRequest.java
+++ blog-rest/src/main/java/com/mizzle/blogrest/payload/request/auth/RefreshTokenRequest.java
@@ -1,6 +1,7 @@
 package com.mizzle.blogrest.payload.request.auth;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -12,6 +13,7 @@ public class RefreshTokenRequest {
 
     @Schema( type = "string", example = "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2NTI3OTgxOTh9.6CoxHB_siOuz6PxsxHYQCgUT1_QbdyKTUwStQDutEd1-cIIARbQ0cyrnAmpIgi3IBoLRaqK7N1vXO42nYy4g5g", description="refresh token 입니다." )
     @NotBlank
+    @NotNull
     private String refreshToken;
 
     public RefreshTokenRequest(){}
